702a6e178a2a2331d84fe40dd4d4e3beb87bab2c
Dillon <dgriffey@datto.com>
2024-01-29 14:38:42 +0000

    added departments and group stats

---
+++ zendesk/livechat_list_departments.go
@@ -0,0 +1,52 @@
+package zendesk
+
+import (
+	"context"
+	"net/http"
+)
+
+type ChatDepartmentsService struct {
+	client *client
+}
+
+type Department struct {
+	Description *string  `json:"description"`
+	Members     *[]int64 `json:"members"`
+	Enabled     bool     `json:"enabled"`
+	ID          int64    `json:"id"`
+	Settings    struct {
+		ChatEnabled    bool   `json:"chat_enabled"`
+		SupportGroupID *int64 `json:"support_group_id"`
+	} `json:"settings"`
+	Name *string `json:"name"`
+}
+
+// https://developer.zendesk.com/api-reference/live-chat/real-time-chat-api/rest/#data-initialization
+
+func (s *ChatDepartmentsService) List(ctx context.Context, pageHandler func(page []Department) error) error {
+
+	requestURL := "https://www.zopim.com/api/v2/departments"
+
+	for {
+		target := []Department{}
+
+		request, err := http.NewRequestWithContext(
+			ctx,
+			http.MethodGet,
+			requestURL,
+			http.NoBody,
+		)
+		if err != nil {
+			return err
+		}
+
+		if err := s.client.LiveChatRequest(request, &target); err != nil {
+			return err
+		}
+
+		if err := pageHandler(target); err != nil {
+			return err
+		}
+
+	}
+}
