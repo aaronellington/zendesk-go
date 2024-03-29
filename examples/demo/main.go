0dc05df99dfe09af95ae98bcada0ebede097ca40
Aaron Ellington <aaron@ellington.io>
2024-03-29 14:52:24 -0400

    wip

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -2,65 +2,28 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"os"
 
 	"github.com/aaronellington/zendesk-go/zendesk"
 )
 
-func prettyPrint(v any) error {
-	bytes, err := json.MarshalIndent(v, "", "\t")
-	if err != nil {
-		return err
-	}
-
-	os.Stdout.Write(bytes)
-	os.Stdout.WriteString("\n")
-
-	return nil
-}
-
-func PrintErr(err error) {
-	if err == nil {
-		return
-	}
-
-	zdErr, ok := err.(*zendesk.Error)
-	if ok {
-		errBytes, _ := io.ReadAll(zdErr.Response.Body)
-		log.Printf("Zendesk Error: [%d] %s", zdErr.Response.StatusCode, string(errBytes))
-		return
-	}
-
-	log.Print(err)
-}
-
 func main() {
 	ctx := context.Background()
 
-	z := zendesk.NewService(
+	z := zendesk.New(
 		os.Getenv("ZENDESK_DEMO_SUBDOMAIN"),
 		zendesk.AuthenticationToken{
 			Email: os.Getenv("ZENDESK_DEMO_EMAIL"),
 			Token: os.Getenv("ZENDESK_DEMO_TOKEN"),
 		},
-		zendesk.ChatCredentials{
-			ClientID:     os.Getenv("ZENDESK_DEMO_CHAT_CLIENT_ID"),
-			ClientSecret: os.Getenv("ZENDESK_DEMO_CHAT_CLIENT_SECRET"),
-		},
 		zendesk.WithLogger(log.New(os.Stdout, "Zendesk API - ", log.LstdFlags)),
 	)
 
-	if err := z.Support().MacroService().List(ctx, func(response zendesk.MacrosResponse) error {
-		for _, entity := range response.Macros {
-			fmt.Println(entity.ID)
-		}
-
-		return nil
-	}); err != nil {
-		PrintErr(err)
+	ticketResponse, err := z.Ticketing().Tickets().Show(ctx, 2)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	log.Println(ticketResponse.Ticket.ID)
 }
